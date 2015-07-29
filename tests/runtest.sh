3f91c8bf36fa90b77e349aaee865cadb8007925c
jaseg <code@jaseg.net>
2015-07-29 15:25:36 +0200

    Tests: add error handling

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -7,12 +7,14 @@ TEST=$3
 
 LOGFILE=$TEST.$TOOL.log
 OUTFILE=$TEST.$TOOL.out
+ERRFILE=$TEST.$TOOL.err
 
-$TOOL ../ribosome.$EXT $TEST.$EXT.dna >$OUTFILE
+$TOOL ../ribosome.$EXT $TEST.$EXT.dna >$OUTFILE 2>$ERRFILE || 
+    { env echo -en "[\033[91mERROR\033[0m ($ERRFILE)] "; exit; }
 
 if diff $OUTFILE $TEST.check >$LOGFILE; then
 	env echo -en "[\033[92mOK\033[0m] "
-	rm $LOGFILE $OUTFILE
+	rm $LOGFILE $OUTFILE $ERRFILE
 else
-	env echo -en "[\033[91mFAIL\033[0m ($LOGFILE)] "
+	env echo -en "[\033[93mFAIL\033[0m ($LOGFILE)] "
 fi
